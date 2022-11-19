5f2741577de1f1099a204439df2486e36ba76d5a
Ikl0 <klimovs2008van@gmail.com>
2022-11-20 01:49:41 +0300

    add routes to the main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
+import Navbar from "./components/Navbar"
+import "./App.css" 
+import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom'
+import Home from "./components/screens/Home"
+import Signin from "./components/screens/Login"
+import Profile from "./components/screens/Profile"
+import Signup from "./components/screens/Signup"
+
 
 function App() {
   return (
-    <h1>hello</h1>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Signin />}/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/profile" element={<Profile />}/>
+        </Routes>
+    </BrowserRouter>
+
   );
 }
 
