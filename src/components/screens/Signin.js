217488f85ed823a450fa97851a900ed5a0660d32
Ikl0 <klimovs2008van@gmail.com>
2022-11-20 02:34:46 +0300

    add Sign In page

---
+++ src/components/screens/Signin.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+const SignIn = () => {
+    return (
+        <div className = "mycard">
+            <div className="card auth-card input-field ">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="Password"></input>
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Sign In</button>
+                <h5><Link to="/signup">Don`t have an account?</Link></h5>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn
