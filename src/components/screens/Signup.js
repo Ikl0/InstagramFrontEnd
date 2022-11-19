a82a8a046441dd55f309c74ac7e309a291612cc8
Ikl0 <klimovs2008van@gmail.com>
2022-11-20 02:34:56 +0300

    add Sign up page

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
-const Signup =()=>{
+const SignUp = () => {
     return (
-        <h1>Signup</h1>
+        <div className = "mycard">
+            <div className="card auth-card input-field ">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="Name"></input>
+                <input type="text" placeholder="Email"></input>
+                <input type="text" placeholder="Password"></input>
+                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Sign Up</button>
+                <h5><Link to="/signin">Already have an account?</Link></h5>
+            </div>
+        </div>
     )
 }
 
-export default Signup
+export default SignUp
