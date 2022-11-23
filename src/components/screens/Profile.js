89cb7cf558d2e732b96f1b9f582982ed6d85cfec
Ikl0 <klimovs2008van@gmail.com>
2022-11-23 16:06:43 +0300

    add profile page

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,8 +1,37 @@
 import React from "react";
 
-const Profile =()=>{
+const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-around",
+                margin: "18px 0px",
+                borderBottom: "1px solid grey"
+            }}>
+                <div>
+                    <img style={{ width: "160px", height: "160px", borderRadius: "80px" }} src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"></img>
+                </div>
+                <div>
+                    <h4>Donald Trump</h4>
+                    <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
+                        <h6>40 posts</h6>
+                        <h6>40 followers</h6>
+                        <h6>40 following</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div className="gallery">
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+                <img className="item" src="https://www.zocalopublicsquare.org/wp-content/uploads/2016/04/Mathews-on-Trump-LEAD.jpg"/>
+            </div>
+        </div>
     )
 }
 
