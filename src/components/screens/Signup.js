1427fb3547e39f6a0b9c2ee12f7771bac242b301
Ikl0 <klimovs2008van@gmail.com>
2022-11-24 16:28:15 +0300

    add method for signup

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -1,15 +1,45 @@
-import React from "react";
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom'
+import M from 'materialize-css'
 
 const SignUp = () => {
+    const navigate = useNavigate()
+
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
+    const postData = () => {
+        fetch("/signup", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+                name,
+                email,
+                password
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            if(data.error){
+                M.toast({html: data.error, classes:"#e53935 red darken-1"})
+            }
+            else {
+                M.toast({html: data.message, classes:"#43a047 green darken-1"})
+                navigate('/signin')
+            }
+        })
+    }
+
     return (
-        <div className = "mycard">
+        <div className="mycard">
             <div className="card auth-card input-field ">
                 <h2>Instagram</h2>
-                <input type="text" placeholder="Name"></input>
-                <input type="text" placeholder="Email"></input>
-                <input type="text" placeholder="Password"></input>
-                <button className="btn waves-effect waves-light #64b5f6 blue darken-1">Sign Up</button>
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1" onClick={()=>postData()}>Sign Up</button>
                 <h5><Link to="/signin">Already have an account?</Link></h5>
             </div>
         </div>
