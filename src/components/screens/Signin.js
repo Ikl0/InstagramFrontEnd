447c9c750c6546ebe0ecab1f89d3b5c04e3820a4
Ikl0 <klimovs2008van@gmail.com>
2022-11-24 12:55:04 +0300

    change button color

--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -8,7 +8,7 @@ const SignIn = () => {
                 <h2>Instagram</h2>
                 <input type="text" placeholder="Email"></input>
                 <input type="text" placeholder="Password"></input>
-                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Sign In</button>
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1">Sign In</button>
                 <h5><Link to="/signup">Don`t have an account?</Link></h5>
             </div>
         </div>
