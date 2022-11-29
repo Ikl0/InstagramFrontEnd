1f90661b1a25b63b17bd5f6bb841d701e485057e
Ikl0 <klimovs2008van@gmail.com>
2022-11-29 18:30:34 +0300

    rewrite inputs

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -37,8 +37,8 @@ const SignUp = () => {
             <div className="card auth-card input-field ">
                 <h2>Instagram</h2>
                 <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button className="btn waves-effect waves-light #64b5f6 blue darken-1" onClick={()=>postData()}>Sign Up</button>
                 <h5><Link to="/signin">Already have an account?</Link></h5>
             </div>
