43868f8fe8f1f0067741e17ef1858cb162918f74
Ikl0 <klimovs2008van@gmail.com>
2022-11-29 18:32:47 +0300

    rewtite logic, add context and routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useEffect, createContext, useReducer, useContext } from "react";
 import Navbar from "./components/Navbar"
-import "./App.css" 
-import {BrowserRouter, Switch, Route, Routes} from 'react-router-dom'
+import "./App.css"
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import Home from "./components/screens/Home"
 import Signin from "./components/screens/Signin"
 import Profile from "./components/screens/Profile"
 import Signup from "./components/screens/Signup"
 import CreatePost from "./components/screens/CreatePost"
+import { reducer, initialState } from './reducers/userReducer'
 
+export const UserContext = createContext()
+
+const Routing = () => {
+  const navigate = useNavigate()
+  const {state,dispatch} = useContext(UserContext)
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    if (user) {
+      dispatch({type:"USER", payload:user})
+    } else {
+      navigate('/signin')
+    }
+  }, [])
+  return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
+  )
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
-    <BrowserRouter>
-    <Navbar />
-    <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<Signin />}/>
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/create" element={<CreatePost />}/>
-        </Routes>
-    </BrowserRouter>
-
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Navbar />
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
