8a5f6b43b52ad508fc329bc40ac8e78b39c170a5
Ikl0 <klimovs2008van@gmail.com>
2022-11-29 19:54:41 +0300

    add comments for the posts

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -62,6 +62,31 @@ const Home = () => {
             })
     }
 
+    const makeComment = (text,postId)=>{
+        fetch('/comment',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer " + localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                postId,
+                text
+            })
+        }).then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            const newData = data.map(item=>{
+                if(item._id == result._id){
+                    return result
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        })
+    }
+
     return (
         <div className="home">
             {
@@ -85,7 +110,19 @@ const Home = () => {
                                 <h6>{item.likes.length} likes</h6>
                                 <h6>{item.title}</h6>
                                 <p>{item.body}</p>
+                                {
+                                    item.comments.map(record=>{
+                                        return(
+                                            <h6 key={record._id}><span style={{fontWeight:"500"}}>{record.postedBy.name}</span> {record.text}</h6>
+                                        )
+                                    })
+                                }
+                                <form onSubmit={(e)=>{
+                                    e.preventDefault()
+                                    makeComment(e.target[0].value,item._id)
+                                }}>
                                 <input type="text" placeholder="add comment" />
+                                </form>
                             </div>
                         </div>
                     )
