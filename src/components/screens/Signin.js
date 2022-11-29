eacce82f502a3bded1dff344c70357be97fc0b5c
Ikl0 <klimovs2008van@gmail.com>
2022-11-29 18:30:49 +0300

    add context

--- src/components/screens/Signin.js
+++ src/components/screens/Signin.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {Link, useNavigate} from 'react-router-dom'
+import {UserContext} from '../../App'
 import M from 'materialize-css'
 
 const SignIn = () => {
+    const {state,dispatch} = useContext(UserContext)
     const navigate = useNavigate()
 
     const [password, setPassword] = useState("")
@@ -24,6 +26,9 @@ const SignIn = () => {
                 M.toast({html: data.error, classes:"#e53935 red darken-1"})
             }
             else {
+                localStorage.setItem("jwt", data.token)
+                localStorage.setItem("user", JSON.stringify(data.user))
+                dispatch({type:"USER", payload:data.user})
                 M.toast({html: "Signed in", classes:"#43a047 green darken-1"})
                 navigate('/')
             }
@@ -33,8 +38,8 @@ const SignIn = () => {
         <div className = "mycard">
             <div className="card auth-card input-field ">
                 <h2>Instagram</h2>
-                <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button className="btn waves-effect waves-light #64b5f6 blue darken-1" onClick={()=>postData()}>Sign In</button>
                 <h5><Link to="/signup">Don`t have an account?</Link></h5>
             </div>
